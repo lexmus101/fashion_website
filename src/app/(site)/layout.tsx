d1ad257bd20e29fd09c42ac53d224bac2325e712
Brian Davidson <brian123513@gmail.com>
2025-09-14 20:52:31 -0700

    Hero

---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from 'next/font/google';
+import '../../styles/globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = ({
+  title: 'Rope',
+  description: 'create by Next.js cohort',
+});
+
+export default function Rootlayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body
+      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
+
---
+++ src/components/Hero/hero.tsx
@@ -0,0 +1,40 @@
+'use-client'
+
+import { ArrowRight } from 'lucide-react'
+
+const Hero = () => {
+    return (
+        <section className='relative h-screen flex items-center justify-center overflow-hidden'>
+            {/* background img */}
+            <div className='absolute inset-0 z-0'>
+                <img
+                src=''
+                alt='Fashion Hero Item'
+                className='w-full h-full object-cover'
+                />
+                <div className='absolute inset-0 bg-black/30'/>
+            </div>
+            
+            {/* content */}
+
+            <div className='relative z-10 text-center text-white max-w-4xl mx-auto px-4'>
+                <h1 className='text-5xl md:text-7xl font-bold mb-6'>
+                        Elevate Your
+                    <br/>
+                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-white 
+                      to-gray-300'>
+                        Style
+                    </span>
+                </h1>
+                <p className='text-xl md:text-2xl mb-8 text-gray-200 max-w-2xl mx-auto'>
+                    Discover our hand chosen collection of premium fashion pieces designed for the modern
+                    person 
+                </p>
+                <div className=''/>
+            </div>
+
+        </section>
+    )
+}
+
+export default Hero;
